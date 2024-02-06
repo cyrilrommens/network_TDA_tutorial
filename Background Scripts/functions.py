1d4e274afc98f1a7204c78f0d1091cd6bc3f498a
cyrilrommens <cyrilrommens@gmail.com>
2024-02-06 21:42:05 +0100

    Update on the energy calculation applied to the data

--- Background Scripts/functions.py
+++ Background Scripts/functions.py
@@ -74,3 +74,115 @@ def generate_overlap_matrix(sets_list):
                 overlap_matrix[i, j] = 1
     
     return overlap_matrix
+
+
+# Function to generate the inverse of the matrix given as input
+def inverse_matrix_generator(matrix):
+
+    # Find non-zero rows and columns
+    non_zero_rows = ~np.all(matrix == 0, axis=1)
+
+    # Store the indices of removed rows. This is the same for the columns since the matrices are symmetric
+    removed_rows = np.where(~non_zero_rows)[0].tolist()
+
+    # Extract the non-zero rows and columns
+    result_matrix = matrix[non_zero_rows][:, non_zero_rows]
+
+    # Generate inverse matrix
+    if np.linalg.det(result_matrix) != 0:
+        inverse_matrix_unrounded = np.linalg.inv(result_matrix)
+        inverse_matrix = np.round(inverse_matrix_unrounded, decimals=2)
+    else:
+        inverse_matrix = 0
+
+    return result_matrix, removed_rows, inverse_matrix
+
+
+# Define function to decompress the compressed array with inserting zeros at the indices of the removed rows and columns
+def decompress_row(array_length, index_removed):
+    
+    # Add +1 to each value in the index_list since the count starts at zero
+    index_removed = [x + 1 for x in index_removed]
+
+    # Create a complete array from 1 to 14
+    complete_array = np.arange(1, 15)
+
+    # Set every row index that is removed to zero in the complete array
+    for index in complete_array:
+        if index in index_removed:
+            complete_array[index-1] = 0
+
+    array_decompressed = complete_array
+    return array_decompressed
+
+
+# Add a zero column at the desired index
+def add_zero_array(original_array, index, axis_choice):
+    
+    # Create a zero array of fitting shape
+    zero_array = np.zeros(original_array.shape[0], dtype=original_array.dtype)
+
+    # Insert the array into the original array
+    inserted_array = np.insert(original_array, index, zero_array, axis=axis_choice)
+
+    return inserted_array
+
+
+# Decompress the matrix to match the shape with the probability matrix L_p for later multiplication
+def decompress(matrix, array_length, removed_rows):
+    
+    # Decompress the 1D array
+    decompressed_1D_array = decompress_row(array_length, removed_rows)
+
+    # Insert zero rows/columns at the indices with value zero from the decompressed 1D array
+    for index in range(0, len(decompressed_1D_array)):
+        if decompressed_1D_array[index] == 0:
+            inserted_column_array = add_zero_array(matrix, index, 0) # The column axis is 0
+            inserted_row_array = add_zero_array(inserted_column_array, index, 1) # The row axis is 1
+            matrix = inserted_row_array
+    return matrix
+
+
+# Compute the internal energy from the inverse connection matrix and the probability matrix as suggested by Knill
+def internal_energy(inverse_connection_matrix, probability_matrix):
+
+    # Compute the internal energy
+    U = np.sum(inverse_connection_matrix * probability_matrix)
+    return U
+
+
+# Compute the entropy from the connection matrix and the probability matrix as suggested by Knill
+def entropy(connection_matrix, probability_matrix):
+
+    # Generate probability matrix specific to the simplicial complex G_i
+    specific_probability_matrix = connection_matrix * probability_matrix
+
+    # Extract the diagonal values as a list
+    diagonal = np.round(np.diag(specific_probability_matrix).tolist(), decimals=2)
+
+    non_zero_diagonal = [value for value in diagonal if value != 0]
+
+    # Compute the entropy
+    S = - np.sum(non_zero_diagonal * np.log(non_zero_diagonal))
+    
+    return S
+
+
+# Compute the free energy from the internal energy and the entropy
+def free_energy(internal_energy, temperature, entropy):
+    return internal_energy - temperature*entropy
+
+
+# Compute the functionals from the dataset
+def functionals(G_matrices, L_matrices, L_p, temperature):
+    U_list = []
+    S_list = []
+    F_list = []
+    for matrix in range(0, len(L_matrices)):
+        U = internal_energy(L_matrices[matrix], L_p)
+        U_list.append(U)
+        S = entropy(G_matrices[matrix], L_p)
+        S_list.append(S)
+        F = free_energy(U, temperature, S)
+        F_list.append(F)
+    return U_list, S_list, F_list
